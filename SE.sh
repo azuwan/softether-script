7dba80f1eae2791286448e4cf2f250946d63e07d
azuwan <my@azuwan.com>
2020-06-19 02:48:00 +0800

    Update SE.sh

--- SE.sh
+++ SE.sh
@@ -7,7 +7,7 @@
 apt-get -y update && apt-get -y upgrade
 
 #Install essentials
-apt-get -y install build-essential libssl-dev g++ openssl libpthread-stubs0-dev gcc-multilib dnsmasq
+apt-get -y install build-essential libssl-dev g++ openssl libpthread-stubs0-dev gcc-multilib
 
 #Backup dnsmasq conf
 mv /etc/dnsmasq.conf /etc/dnsmasq.conf-bak
@@ -40,4 +40,43 @@ chmod 700 vpnserver
 #Create systemd init file for Softether VPN service
 wget -O /lib/systemd/system/vpnserver.service https://whattheserver.me/softether-scripts/vpnserver.service
 
-echo "Complete"
+# install dropbear
+apt-get -y install dropbear
+sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=3128/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 143"/g' /etc/default/dropbear
+echo "/bin/false" >> /etc/shells
+echo "/usr/sbin/nologin" >> /etc/shells
+service ssh restart
+service dropbear restart
+
+# install squid3
+cd
+apt-get -y install squid3
+wget -O /etc/squid3/squid.conf "https://raw.githubusercontent.com/Clrkz/VPSAutoScrptz/master/squid3.conf"
+sed -i $MYIP2 /etc/squid3/squid.conf;
+service squid3 restart
+
+# install stunnel
+apt-get install stunnel4 -y
+cat > /etc/stunnel/stunnel.conf <<-END
+cert = /etc/stunnel/stunnel.pem
+client = no
+socket = a:SO_REUSEADDR=1
+socket = l:TCP_NODELAY=1
+socket = r:TCP_NODELAY=1
+
+
+[dropbear]
+accept = 443
+connect = 127.0.0.1:3128
+
+#konfigurasi stunnel
+sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
+/etc/init.d/stunnel4 restart
+
+# finishing
+service dropbear restart
+service squid3 restart
+
+END
